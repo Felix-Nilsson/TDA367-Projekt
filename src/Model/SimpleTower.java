4a442dfa22c59d59a05eb7349e973ee554a0bbb9
Felix Nilsson <felix.nilsson.se@gmail.com>
2020-09-10 14:26:58 +0200

    Added a variable that points to a base tower from SimpleTower

--- src/Model/SimpleTower.java
+++ src/Model/SimpleTower.java
@@ -3,8 +3,12 @@ package Model;
 public class SimpleTower implements Tower{
     BaseTower baseTower;
 
+    public SimpleTower(){
+        baseTower = new BaseTower();
+    }
+
     @Override
     public void update() {
-
+        baseTower.update();
     }
 }
