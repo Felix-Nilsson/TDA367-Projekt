4962bc3d86c176c66451ae5b017098082d10f5d7
Oscar <44546586+SevenDeadlySloths@users.noreply.github.com>
2020-09-15 10:08:05 +0200

    SetStage

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,27 @@
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.scene.Parent;
+
+
 
 
 public class Main extends Application{
-    public static Stage stage;
 
-    public void start(Stage primaryStage) throws Exception{
-        stage = primaryStage;
-        System.setProperty("user.home", System.getProperty("user.dir"));
-        primaryStage.setTitle("Tower Defense");
-        //TODO Get instance of the startview(menu)
-        primaryStage.setResizable(false);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("View/Map.fxml"));
+
+        Scene scene = new Scene(root,600,600);
+        stage.setTitle("Tower Defense");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //TODO Backend when we implement it
-            }
-        }));
 
     }
+
 }
