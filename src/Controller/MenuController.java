bdea7d17f29e6c2ad310ff30c44ef6ddcadfb8c8
JonEmilsson <57443386+JonEmilsson@users.noreply.github.com>
2020-09-19 10:21:39 +0200

    Mainmenu added

--- src/Controller/MenuController.java
+++ src/Controller/MenuController.java
@@ -1,4 +1,24 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
 public class MenuController {
+    @FXML private Button newGameButton;
+    @FXML private Button loadGameButton;
+    @FXML private Button optionsButton;
+    @FXML private Button creditsButton;
+    @FXML private Button exitGameButton;
+    // HEJ4
+    @FXML
+    private void newGame(){ }
+    @FXML
+    private void loadGame(){ }
+    @FXML
+    private void options(){ }
+    @FXML
+    private void credits(){ }
+    @FXML
+    private void exitGame(){ }
 }
