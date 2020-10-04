65e1624af39d39416c4d681d4cc576634b864841
silarss <zico.xp.larsson@gmail.com>
2020-10-04 02:12:38 +0200

    added functionality for Projectile

--- src/Model/Projectile.java
+++ src/Model/Projectile.java
@@ -1,16 +1,74 @@
 package Model;
 
-public class Projectile {
-    private int vx;
-    private int vy;
-    private int posX;
-    private int posY;
+import Model.Towers.Tower;
+import javafx.scene.image.Image;
 
-    public Projectile(){
+public class Projectile implements Updatable{
+    private double vx;
+    private double vy;
+    private double posX;
+    private double posY;
+    private double enemyPosX;
+    private double enemyPosY;
+    private int angle;
+    private int vMultiplier=10;
+    Image image;
+
+    //Kan också testa med Enemy som arg istället. Blir lättare om projectiles inte kan missa eftersom man då har direkt tillgång till Enemy.
+    //Om projectile inte är hitscan och tower har en Angle blir det lätt att implementera en riktig projektil (collision måste dock skapas då)
+    public Projectile(double towerPosX, double towerPosY, int enemyPosX, int enemyPosY){
+
+        this.posX=towerPosX;
+        this.posY=towerPosY;
+        this.enemyPosX=enemyPosX;
+        this.enemyPosY=enemyPosY;
+
+        //this.angle=tower.getAngle();
+        calculateVelocity();
+
+        this.image = new Image((getClass().getClassLoader().getResourceAsStream("img/bluemonster1.png")));
 
     }
-    private void attackEnemy(Enemy enemy){
-        
+    private void damageEnemy(Enemy enemy){
+        //temporärt
+        enemy.tookDamage(5);
+    }
+    private void disappearIfHit(){
+        if (isColission()){
+
+        }
+    }
+    private void createImage(){
+
+    }
+    private void imageToPos(){
+
+    }
+    private void calculateVelocity(){
+        //Om det finns en angle i Tower:
+        vx = vMultiplier*Math.cos(angle);
+        vy = vMultiplier*Math.sin(angle);
+
+        //Om det inte finns:
+        double distX = enemyPosX-posX;
+        double distY = enemyPosY-posY;
+        double theAngle = Math.atan2(distY,distX);
+        vx = vMultiplier*Math.cos(theAngle);
+        vy = vMultiplier*Math.sin(theAngle);
+    }
+    private void move(){
+        posX=posX+vx;
+        posY=posY+vy;
+    }
+
+    private boolean isColission(){
+        //temporärt
+        return false;
+    }
+
+    public void update(){
+        move();
+        disappearIfHit();
     }
 
 }
