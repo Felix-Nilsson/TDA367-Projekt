5c5d67baf60e220637063d118bbb1a63de45a5a8
Oscar <44546586+SevenDeadlySloths@users.noreply.github.com>
2020-10-16 12:16:47 +0200

    testfix

--- Test/Model/BoardTest.java
+++ Test/Model/BoardTest.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 class BoardTest {
 
     @Test
     void fillPath() {
-        //båda dessa ska inte behöva finnas med egentligen
         Board b= new Board(1);
-    /*
+
         List<BaseEnemy.Direction> path = b.getPath();
         assertEquals(BaseEnemy.Direction.EAST, path.get(0));
         assertEquals(BaseEnemy.Direction.EAST, path.get(7));
@@ -20,7 +21,7 @@ class BoardTest {
         assertEquals(BaseEnemy.Direction.SOUTH, path.get(93));
         assertEquals(BaseEnemy.Direction.EAST, path.get(110));
         System.out.println("start: " + path.get(0) +" path(1): "+ path.get(1) +" path(2): "+ path.get(2) +" path(24): "+ path.get(24)+" path(30): "+ path.get(30));
-*/
+
     }
 
 
