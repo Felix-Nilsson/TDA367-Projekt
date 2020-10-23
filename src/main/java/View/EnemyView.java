560fa54266581d815e6df50d92f08863db892dc1
Hannes Svahn <57436847+Hemlos@users.noreply.github.com>
2020-10-23 16:25:26 +0200

    Fixed small typo :))

--- src/main/java/View/EnemyView.java
+++ src/main/java/View/EnemyView.java
@@ -12,8 +12,8 @@ public class EnemyView {
 
     public Image getImage() throws Exception {
         switch (enemy.getId()){
-            case 1 : return new Image("/img/blue_Monster.png");
-            case 2 : return new Image("/img/red_monster.png");
+            case 1 : return new Image("img/blue_Monster.png");
+            case 2 : return new Image("img/red_Monster.png");
         }
 
         throw new Exception("Invalid id");
